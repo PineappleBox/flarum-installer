eff307ee6b559e365c0a6970541f06ae8ab7a53f
Hexa <mail@hexatech.gq>
2017-10-03 22:32:02 -0500

    Added New Welcome Screen

--- index.php
+++ index.php
@@ -1,1 +1,92 @@
-Welcome! Click <a href="install">here</a> to continue.
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Welcome to the Auto-Installer! | Hexa</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1">
+    <link rel="shortcut icon" href="https://myhexa.co/back/favicon.ico">
+    <link rel="stylesheet" type="text/css" href="https://myhexa.co/scss/bootstrap/bootstrap.css" />
+    <link rel="stylesheet" type="text/css" href="https://myhexa.co/dist/theme.min.css" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"></script>
+    <script src="https://myhexa.co/dist/theme.min.js"></script>  
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.min.css">
+    </head>
+
+<body class="signin-page">
+                  <nav class="navbar navbar-expand-lg navbar-dark fixed-top" role="navigation">
+                <div class="container">
+                  <button class="navbar-toggler" data-toggle="collapse" data-target="#navbar-collapse-2">
+                    <span class="navbar-toggler-icon"></span>
+                  </button>
+
+                  <a class="navbar-brand" href="#"><img src="img/logo.png" class="d-none d-lg-inline mr-2 w-25" /></a>
+
+                  <div class="collapse navbar-collapse justify-content-end" id="navbar-collapse-2">
+                    <ul class="navbar-nav">
+                      <li class="nav-item">
+                        <a class="nav-link" href="https://myhexa.co" target="_blank">Home</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="https://myhexa.co/features" target="_blank">Features</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="https://myhexa.co/pricing" target="_blank">Pricing</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="https://myhexa.co/contact" target="_blank">Contact Us</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="https://community.myhexa.co" target="_blank">Community</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link nav-link--rounded ml-lg-2" target="_blank" href="http://flarum-demo.myhexa.co">View Demo Site</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </nav>
+
+
+
+        <br><br><br><br><br><br><div class="wrapper"><h1>Getting Started</h1><br>
+<p>Welcome to the Hexa auto-installer! By clicking continue, you will be taken to the install screen.<br> There, you will need a MySQL Database and a MySQL User. If you do not have these, please create them and then continue on.<br><br><i><b>Sharing is Caring. Please Share This!</i></b></div>
+
+
+
+
+<div class="wrapper">
+  <div class="FormButtons">
+<button type="submit" class="btn-shadow btn-shadow-dark btn-pill" onclick="window.location.href='install'">CONTINUE TO INSTALL <i class="zmdi zmdi-arrow-right"></i></button>
+  
+  </div>
+  </div>
+  <br><br>
+<div class="index-features-cta">
+  <div class="container">
+    <div class="info">
+      <strong>
+At Hexa, we believe code is AWESOME!
+</strong>
+      <p>
+        It takes a lot of time and hard work to put together these projects.<br> Please show your support by sharing this project!
+      </p>
+    </div>
+    <a href="https://twitter.com/home?status=I%20just%20installed%20%40flarum%20using%20the%20%40myhexa%20auto-installer.%20You%20should%20really%20check%20out%20their%20stuff!%20It's%20really%20AWESOME!!!">Share on Twitter</a>
+  </div>
+</div>
+
+<div class="wrapper"><br>
+  <div class="alert alert-inverse" role="alert">
+  Running into any issues during installation? Please click <a href="https://community.myhexa.co/t/flarum-installer">here</a> to let us know about them.
+</div>
+
+
+
+
+<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+      </div>
+    </div>
+  </body>
+</html>
